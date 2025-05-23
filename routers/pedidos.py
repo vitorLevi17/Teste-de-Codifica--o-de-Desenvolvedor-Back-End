cc2b0144dd8915b996416b946c03645ebb6d14b4
vitorLevi17 <vitorlevimenezessantos@outlook.com>
2025-05-23 13:27:43 -0300

    Endpoint de deleção de pedidos

--- routers/pedidos.py
+++ routers/pedidos.py
@@ -19,6 +19,16 @@ def pedidos(db:SessionLocal = Depends(get_db)):
     pedidos = db.query(models.Pedidos).all()
     return pedidos
 
+@router.get('/{pedidos_id}',response_model=PedidoSchema)
+def pedido_id(pedidos_id:int ,db:SessionLocal = Depends(get_db)):
+    pedido = db.query(models.Pedidos).filter(models.Pedidos.id == pedidos_id).first()
+    if pedido is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Pedido com ID{pedidos_id} não encontrado"
+        )
+    return pedido
+
 @router.post('/',response_model=CriarPedidoSchema)
 def criar_pedido(pedidos: CriarPedidoSchema,db:SessionLocal = Depends(get_db)):
     pedidos = models.Pedidos(**pedidos.dict(),status="Em preparação")
