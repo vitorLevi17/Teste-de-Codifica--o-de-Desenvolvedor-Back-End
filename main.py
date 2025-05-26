61d6087248ed47145339388b801a621a694894a6
vitorLevi17 <vitorlevimenezessantos@outlook.com>
2025-05-26 09:51:57 -0300

    Alterar nome dos endpoints e proteger rotas de produtos

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+
+from models import models
 from routers import cliente, produto,pedidos,usuarios
 from models.database import engine
 from models.models import Base
+from auxiliars.usuario_token import get_current_user
 app = FastAPI()
 Base.metadata.create_all(bind=engine)
 
-app.include_router(cliente.router,prefix='/cliente',tags=['Clientes'])
-app.include_router(produto.router,prefix='/produto',tags=['Produtos'])
-app.include_router(pedidos.router,prefix='/pedidos',tags=['Pedidos'])
-app.include_router(usuarios.router,prefix='/usuarios',tags=['Usuarios'])
+app.include_router(cliente.router,prefix='/clients',tags=['Clientes'])
+app.include_router(produto.router,prefix='/products',tags=['Produtos'])
+app.include_router(pedidos.router,prefix='/orders',tags=['Pedidos'])
+app.include_router(usuarios.router,prefix='/auth',tags=['Usuarios'])
 
 @app.get('/')
-def index():
-    return "hello world"
+def index(current_user: models.Users = Depends(get_current_user)):
+    endpoints = [
+        '127.0.0.1:8000/clients',
+        '127.0.0.1:8000/products',
+        '127.0.0.1:8000/orders',
+        '127.0.0.1:8000/auth'
+    ]
+    return endpoints
--- routers/produto.py
+++ routers/produto.py
@@ -5,22 +5,22 @@ from models import models
 from typing import List
 from schemas.produto import ProdutoSchema,ProdutoCriarSchema
 from auxiliars import validacoes
+from auxiliars.usuario_token import get_current_user
 
 router = APIRouter()
-
 def get_db():
     db = SessionLocal()
     try:
         yield db
     finally:
         db.close()
-
 @router.get('/',response_model=List[ProdutoSchema])
 def produto(db: SessionLocal = Depends(get_db),skip: int = Query(0, ge=0),
     limit: int = Query(10,le=100),
     categoria: str = Query(None),
     preco: float = Query(None),
-    disponivel_s_n: str = Query(None)
+    disponivel_s_n: str = Query(None),
+    current_user: models.Users = Depends(get_current_user)
             ):
     query = db.query(models.Produtos)
 
@@ -35,22 +35,22 @@ def produto(db: SessionLocal = Depends(get_db),skip: int = Query(0, ge=0),
     return produtos
 
 @router.get('/{produto_id}',response_model=ProdutoSchema)
-def produto_id(produto_id:int ,db: SessionLocal = Depends(get_db)):
+def produto_id(produto_id:int ,db: SessionLocal = Depends(get_db),
+               current_user: models.Users = Depends(get_current_user)):
     produto = db.query(models.Produtos).filter(models.Produtos.id == produto_id).first()
     validacoes.validar_objeto_bd(produto,produto_id)
     return produto
-
 @router.post('/',response_model=ProdutoSchema)
-def criar_produto(produto: ProdutoCriarSchema,db:Session = Depends(get_db)):
+def criar_produto(produto: ProdutoCriarSchema,db:Session = Depends(get_db),
+                  current_user: models.Users = Depends(get_current_user)):
     produto_novo = models.Produtos(**produto.dict())
     validacoes.validar_produto(produto, db)
     db.add(produto_novo)
     db.commit()
     db.refresh(produto_novo)
     return produto_novo
-
 @router.put('/{produto_id}',response_model=ProdutoSchema)
-def editar_produto(produto_id:int,produto_put:ProdutoCriarSchema,db:Session = Depends(get_db)):
+def editar_produto(produto_id:int,produto_put:ProdutoCriarSchema,db:Session = Depends(get_db),current_user: models.Users = Depends(get_current_user)):
     produto = db.query(models.Produtos).filter(models.Produtos.id == produto_id).first()
 
     validacoes.validar_objeto_bd(produto,produto_id)
@@ -63,9 +63,9 @@ def editar_produto(produto_id:int,produto_put:ProdutoCriarSchema,db:Session = De
     db.commit()
     db.refresh(produto)
     return produto
-
 @router.delete('/{produto_id}')
-def excluir_produto(produto_id: int, db:Session = Depends(get_db)):
+def excluir_produto(produto_id: int, db:Session = Depends(get_db),
+                    current_user: models.Users = Depends(get_current_user)):
     produto = db.query(models.Produtos).filter(models.Produtos.id == produto_id).first()
     validacoes.validar_objeto_bd(produto,produto_id)
     db.delete(produto)
